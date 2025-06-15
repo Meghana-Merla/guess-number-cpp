d1e569a57ce7a23f7ccfbd9e564719fc241a6541
Meghana-Merla <24je0654@iitism.ac.in>
2025-06-15 22:39:20 +0530

    generating number

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
-
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
+using namespace std;
 int getRandomNumber() {
-    static bool seed_initialized = false; // Ensure seed is initialized only once
+    static bool seed_initialized = false;
     if (!seed_initialized) {
         std::srand(std::time(0));
         seed_initialized = true;
@@ -12,15 +11,12 @@ int getRandomNumber() {
 
     int min=1 ;
     int max =100;
-
-    // Generate random number in the range [1, 100]
     int rand_number = (rand() % (max - min + 1)) + min;
     return rand_number;
 }
 
 int main() {
-    // Generate and print random number
-    std::cout << "Random Number: " << getRandomNumber() << std::endl;
+    cout << "Random Number: " << getRandomNumber() << endl;
     
     return 0;
 }
